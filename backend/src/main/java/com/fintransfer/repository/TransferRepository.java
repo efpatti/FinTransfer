7a6ce564b0d45231ea6d45fd2fd8344c07dc71c2
Enzo Patti <enzo.patti@aluno.senai.br>
2025-09-06 14:23:07 -0300

    feat: configure H2 database and create Transfer JPA entity
    - Added H2 console access
    - Configured application.properties for H2 and JPA
    - Created Transfer entity in com.fintransfer.model
    - Added TransferRepository interface in com.fintransfer.repository
    - Verified persistence and retrieval of Transfer objects
    
    Closes #2

---
+++ backend/src/main/java/com/fintransfer/repository/TransferRepository.java
@@ -0,0 +1,9 @@
+package com.fintransfer.repository;
+
+import com.fintransfer.model.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
